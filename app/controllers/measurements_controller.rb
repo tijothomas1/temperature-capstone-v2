af89422a6dcddd1b294c89de9505142094126e36
TIJO THOMAS <tt.opt.ins@gmail.com>
2016-02-24 16:17:24 -0600

    Added new views, adjusted models.

--- app/controllers/measurements_controller.rb
+++ app/controllers/measurements_controller.rb
@@ -1,8 +1,50 @@
 class MeasurementsController < ApplicationController
 
-  def index
+  before_action :authenticate_admin!
 
+  def index
+    @measurements = Measurement.all
   end
 
+  def show
+    @measurement = Measurement.find(params[:id])
+  end
+
+  def new
+    @measurement = Measurement.new
+  end
+
+  def create
+    @measurement = Measurement.create({
+                                        reading: params[:reading]
+                                      })
+      flash[:success] = "Measurement Added"
+
+    redirect_to "/"
+  end
+
+  def edit
+    @measurement = Measurement.find(params[:id])
+  end
+
+  def update
+    @measurement = Measurement.find(params[:id])
+    @measurement.update({
+                          reading: params[:reading]
+                        })
+
+    flash[:success] = "Measurement Updated"
+
+    redirect_to "/measurements/#{@measurement.id}"
+  end
+
+  def destroy
+    @measurement = Measurement.find(params[:id])
+    @measurement.destroy
+
+    flash[:warning] = "Measurement has been DELETED"
+
+    redirect_to "/"
+  end
 
 end
